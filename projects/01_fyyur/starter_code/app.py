05c74527d3fd6b0a282f9cb3fba7754ed5392e40
Rohit Yadav <rohity2304@gmail.com>
2020-05-10 13:09:46 +0530

    Adding __repr__ for models

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -46,7 +46,8 @@ class Venue(db.Model):
     seeking_description = db.Column(db.String(1024))
     shows = db.relationship('Show', backref='venue', lazy=True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+        return "<Venue {}, {}, {}, {}>".format(self.id, self.name, self.city, self.state)
 
 
 class Artist(db.Model):
@@ -65,10 +66,10 @@ class Artist(db.Model):
     seeking_description = db.Column(db.String(1024))
     shows = db.relationship('Show', backref='artist', lazy=True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+        return "<Artist {}, {}, {}, {}>".format(self.id, self.name, self.city, self.state)
 
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = 'Show'
 
@@ -76,6 +77,9 @@ class Show(db.Model):
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return "<Show {}, {}, {}>".format(self.artist_id, self.venue_id, self.start_time)
 
 
 # ----------------------------------------------------------------------------#
