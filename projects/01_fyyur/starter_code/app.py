e8ae318cc5c31e2138b5e81509fa7dfe13e3e9a7
Rohit Yadav <rohity2304@gmail.com>
2020-05-10 16:12:14 +0530

    Replaced mock with real data for artists endpoints

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -151,11 +151,22 @@ def search_venues():
     # search for Hop should return "The Musical Hop".
     # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
     search_term = request.form.get('search_term', '')
-    venue_query = Venue.query.filter(Venue.name.ilike("%{}%".format(search_term))).order_by(Venue.id).all()
+    data = []
+    venue_query = Venue.query.with_entities(Venue.id, Venue.name).filter(
+        Venue.name.ilike("%{}%".format(search_term))
+    ).order_by(Venue.id).all()
+
+    for v in venue_query:
+        obj = {
+            "id": v.id,
+            "name": v.name,
+            "num_upcoming_shows": Show.query.filter(Show.venue_id == v.id, Show.start_time > datetime.now()).count()
+        }
+        data.append(obj)
 
     response = {
         "count": len(venue_query),
-        "data": venue_query
+        "data": data
     }
 
     return render_template('pages/search_venues.html', results=response, search_term=search_term)
@@ -279,113 +290,71 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
-    return render_template('pages/artists.html', artists=data)
+    artist_query = Artist.query.with_entities(Artist.id, Artist.name).order_by(Artist.id).all()
+    return render_template('pages/artists.html', artists=artist_query)
 
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # search for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get('search_term', '')
+    data = []
+    artist_query = Artist.query.with_entities(Artist.id, Artist.name).filter(
+        Artist.name.ilike("%{}%".format(search_term))
+    ).order_by(Artist.id).all()
+
+    for a in artist_query:
+        obj = {
+            "id": a.id,
+            "name": a.name,
+            "num_upcoming_shows": Show.query.filter(Show.artist_id == a.id, Show.start_time > datetime.now()).count()
+        }
+        data.append(obj)
+
     response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+        "count": len(artist_query),
+        "data": data
     }
-    return render_template('pages/search_artists.html', results=response,
-                           search_term=request.form.get('search_term', ''))
+
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
     # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "past_shows": [{
-            "venue_id": 1,
-            "venue_name": "The Musical Hop",
-            "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 5,
-        "name": "Matt Quevedo",
-        "genres": ["Jazz"],
-        "city": "New York",
-        "state": "NY",
-        "phone": "300-400-5000",
-        "facebook_link": "https://www.facebook.com/mattquevedo923251523",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "past_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 6,
-        "name": "The Wild Sax Band",
-        "genres": ["Jazz", "Classical"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "432-325-5432",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "past_shows": [],
-        "upcoming_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 3,
-    }
-    data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+    artist_query = Artist.query.get(artist_id)
+    data = artist_query.__dict__
+    data["genres"] = data["genres"].split(",")
+
+    data["past_shows"] = []
+    data["past_shows_count"] = Show.query.filter(Show.artist_id == artist_query.id,
+                                                 Show.start_time <= datetime.now()).count()
+    data["upcoming_shows"] = []
+    data["upcoming_shows_count"] = Show.query.filter(Show.artist_id == artist_query.id,
+                                                     Show.start_time > datetime.now()).count()
+
+    join_query = Show.query.join(
+        Venue, Venue.id == Show.venue_id
+    ).add_columns(
+        Venue.id.label("venue_id"),
+        Venue.name.label("venue_name"),
+        Venue.image_link.label("venue_image_link"),
+        Show.start_time.label("start_time")
+    )
+
+    if data["past_shows_count"] > 0:
+        data["past_shows"] = join_query.filter(
+            Show.artist_id == artist_query.id,
+            Show.start_time <= datetime.now()
+        ).all()
+
+    if data["upcoming_shows_count"] > 0:
+        data["upcoming_shows"] = join_query.filter(
+            Show.artist_id == artist_query.id,
+            Show.start_time > datetime.now()
+        ).all()
+
     return render_template('pages/show_artist.html', artist=data)
 
 
